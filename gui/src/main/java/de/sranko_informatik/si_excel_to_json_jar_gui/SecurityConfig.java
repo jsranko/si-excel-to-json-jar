78eb10375aca04c60c22dd5ec42e8c2ad884e2b2
Juraj Sranko <31003320+jsranko@users.noreply.github.com>
2021-06-01 19:32:22 +0200

    https enabled

---
+++ gui/src/main/java/de/sranko_informatik/si_excel_to_json_jar_gui/SecurityConfig.java
@@ -0,0 +1,16 @@
+package de.sranko_informatik.si_excel_to_json_jar_gui;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable();
+    }
+}
