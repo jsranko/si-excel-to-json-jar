59316fbf1c5e91f4ebca0aaad66c3beda726d785
Juraj Sranko <31003320+jsranko@users.noreply.github.com>
2021-06-03 12:17:54 +0200

    csrf disabled

--- gui/src/main/java/de/sranko_informatik/si_excel_to_json_jar_gui/GuiApplication.java
+++ gui/src/main/java/de/sranko_informatik/si_excel_to_json_jar_gui/GuiApplication.java
@@ -2,16 +2,11 @@ package de.sranko_informatik.si_excel_to_json_jar_gui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class GuiApplication {
 
-    public static void main(String[] args) {
-
-        SpringApplication application = new SpringApplication(GuiApplication.class);
-        application.setAdditionalProfiles("ssl");
-        application.run(args);
+    public static void main(String[] args) {SpringApplication.run(GuiApplication.class, args);
 
     }
 
--- gui/src/main/java/de/sranko_informatik/si_excel_to_json_jar_gui/SecurityConfig.java
+++ gui/src/main/java/de/sranko_informatik/si_excel_to_json_jar_gui/SecurityConfig.java
@@ -12,5 +12,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable();
+        http.csrf().disable();
     }
 }
