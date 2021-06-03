083847129716342ae212a92363f1098cbd69011d
Juraj Sranko <31003320+jsranko@users.noreply.github.com>
2021-06-03 18:58:32 +0200

    truststore activation

--- gui/src/main/java/de/sranko_informatik/si_excel_to_json_jar_gui/SecurityConfig.java
+++ gui/src/main/java/de/sranko_informatik/si_excel_to_json_jar_gui/SecurityConfig.java
@@ -1,17 +1,30 @@
 package de.sranko_informatik.si_excel_to_json_jar_gui;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private Environment env;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable();
         http.csrf().disable();
+    }
+
+    @PostConstruct
+    private void configureSSL() {
+        System.setProperty("javax.net.ssl.trustStore", env.getProperty("server.ssl.trust-store"));
+        System.setProperty("javax.net.ssl.trustStorePassword",env.getProperty("server.ssl.trust-store-password"));
     }
 }
